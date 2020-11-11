0601e8c4804df81051b2c10a65b01b653d105f65
Tristan Bond <tristanebond@gmail.com>
2020-11-11 14:30:04 -0800

    list buttons redisplay

--- script.js
+++ script.js
@@ -9,10 +9,26 @@ var cardDate=$(".cardDate");
 var cardIcon=$(".cardIcon");
 var cardTemp=$(".cardTemp");
 var cardHumidity=$(".cardHumidity");
-
-
+var latitude;
+var longitude;
+var cityName;
 $(".custom-button").on("click", function(){
-    var cityName=$("#cityNameInput").val();
+    cityName=$("#cityNameInput").val();
+    appendNew(cityName);
+});
+$(".cityList").on("click", function(event){
+    cityName=$(event.target).data("city");
+    displayWeather(cityName);
+})
+function appendNew(cityName){
+    var newItem=$("<li>");
+        newItem.text(cityName);
+        newItem.addClass("list-group-item");
+        newItem.data("city", cityName);
+        cityListEl.append(newItem);
+        displayWeather(cityName);
+}
+function displayWeather(cityName){
     var queryURL="https://api.openweathermap.org/data/2.5/weather?q="+ cityName+",usa&appid=80b777ad3b7652f1518994be4a5ae0fd";
     $.ajax({
         url:queryURL,
@@ -24,21 +40,40 @@ $(".custom-button").on("click", function(){
         tempEl.text("Temperature: " + temp);
         humidityEl.text("Humidity: " + response.main.humidity);
         windSpeedEl.text("Wind Speed: " + response.wind.speed);
-        var newItem=$("<li>");
-        newItem.text(cityName);
-        newItem.addClass("list-group-item");
-        cityListEl.append(newItem);
-       
+        latitude=response.coord.lat;
+        longitude=response.coord.lon;
+
+            $.ajax({
+                url:"http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=80b777ad3b7652f1518994be4a5ae0fd",
+                method:"GET"
+            }).then(function(response){
+                uvIndexEl.text("UV Index: " + response[3].value);
+
+                    if(response[3].value<3){
+                        uvIndexEl.css("background-color", "green");
+                    }else if(response[3].value<6){
+                        uvIndexEl.css("background-color", "yellow");
+                    }else if(response[3].value<8){
+                        uvIndexEl.css("background-color", "orange");
+                    }else if(response[3].value<11){
+                        uvIndexEl.css("background-color", "red");
+                    }else{
+                        uvIndexEl.css("background-color", "purple");
+                    }
+            })
     });
+ 
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=80b777ad3b7652f1518994be4a5ae0fd",
         method:"GET"
     }).then(function(response){
-        console.log(response);
         var j=3;
         for(var i=0;i<5;i++){
             var temp=Math.floor((response.list[j].main.temp_max -273.15) *1.80 +32);
-            $(cardArr[i].find(cardDate)).text(response.list[j].dt_txt);
+            var date=response.list[j].dt_txt.split("");
+            var dateFormat=date[5]+date[6]+date[7]+date[8]+date[9]+date[4]+date[0]+date[1]+date[2]+date[3];
+            
+            $(cardArr[i].find(cardDate)).text(dateFormat);
             $(cardArr[i].children().find(cardTemp)).text("Temp: " + temp);
             $(cardArr[i].children().find(cardHumidity)).text("Humidity: " + response.list[j].main.humidity);
 
@@ -55,10 +90,11 @@ $(".custom-button").on("click", function(){
                 $(cardArr[i].children().find(cardIcon)).addClass("fas fa-sun");
                 $(cardArr[i].children().find(cardIcon)).text("");
             }
+            
             j+=8;
             
                 }
+
     });
-   
-})
+}
 
